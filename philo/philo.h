9edd14780454a86a92f4f1b1a641dd198de5b167
Jan Karras <jkarras@c2r8s4.42wolfsburg.de>
2024-04-29 13:35:11 +0200

    t

---
+++ philo/philo.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkarras <jkarras@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 12:57:36 by jkarras           #+#    #+#             */
+/*   Updated: 2024/04/29 13:34:27 by jkarras          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+
+# define PHILO_H
+
+# include <stdio.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+
+typedef struct s_data
+{
+	int	*philos;
+	int	*forks;
+	int
+}	t_data;
+
+int	main(int argc, char **argv);
+
+#endif
